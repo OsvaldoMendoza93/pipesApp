5c2eea71b19a9884be25b450936e35852b0bce81
OsvaldoMendoza93 <osvaldo.mendoza1249@gmail.com>
2023-03-10 18:36:33 -0600

    pipes no comunes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  nombre: string = 'osvalDo mendoZa';
-  valor: number = 1000;
-  obj = {
-    nombre: 'osvaldo'
+export class AppComponent implements OnInit {
+
+  constructor(private primengConfig: PrimeNGConfig){
   }
 
-  mostrarNombre(){
-    console.log(this.nombre);
-    console.log(this.valor);
-    console.log(this.obj);
+  ngOnInit(): void {
+    this.primengConfig.ripple = true;
   }
 }
